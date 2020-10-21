205f0aa0e336628fd96a01efea1ecafa549454e6
Yonsei-U <73234952+Yonsei-U@users.noreply.github.com>
2020-10-22 05:00:08 +0900

    U

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Button
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import TodayIcon from "@material-ui/icons/Today";
 
 export default () => {
   return (
@@ -14,6 +15,9 @@ export default () => {
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
+        </IconButton>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <TodayIcon />
         </IconButton>
         <Typography variant="h6">News</Typography>
         <Button color="inherit">Login</Button>
