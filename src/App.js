96ae9e7d794dfed4fa6b474880842832703788b6
Yonsei-U <73234952+Yonsei-U@users.noreply.github.com>
2020-10-21 23:01:44 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ export default () => {
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
+        <Typography variant="h6">News</Typography>
+        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
